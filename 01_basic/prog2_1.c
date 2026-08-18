a4767d188b04535d0c19cbc6f2b61a456c29c4ef
catherine20260805 <catherine510202@gmail.com>
2026-08-18 14:28:18 +0800

    Add basic2_1 practice

---
+++ 01_basic/prog2_1.c
@@ -0,0 +1,12 @@
+/*prog2_1,簡單的C語言*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	int num;
+	num=2;
+	printf("I have %d cats.\n",num);
+	printf("You have %d cats, too.\n",num);
+	system("pause");
+	return 0;
+}
